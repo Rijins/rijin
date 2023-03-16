bb54917e3e044e31a4c0d6c6396c0ef00db3bca4
UdithDas <udithdaskm@gmail.com>
2023-03-16 02:28:01 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// declaration
+// let a="10";
+// if(a==10){
+    // console.log("good");
+// }else{
+    // console.log("bad");
+// }
+// let arr=["Madhu","Bunny","Kandhamma","kokkadan"];
+// console.log(arr);
+// arr.push("Kukku");
+// console.log(arr);
+// console.log(arr.length);
+// arr.pop();
+// console.log(arr);
+// console.log(arr[2]);
+
+// let car=[
+//  {
+    // model:"bs12",
+    // color:"sky blue",
+    // price:1000000
+//  },  
+//  {
+    // model:"GTR ",
+    // color:"orange",
+    // price:1100000
+//  },
+//  {
+    // model:"supra",
+    // color:"black",
+    // price:1900000
+//  }
+// ]
+// console.log(car[0].model);
+// let fr=["apple","banana","grapes","orange"];
+//  for(let i=0;i<fr.length;i++){
+//  console.log(fr[i]);
+// }
+// let i=0;
+// do{
+    // console.log(fr[i]);
+    // i=i+1;
+// }while(i<fr.length)
+// function add(x,y){
+    // let sum=0;
+    // sum=x+y;
+    // return(sum);
+// }
+// console.log(add(7,10));
+const multiply=(a,b)=>{
+    console.log(a*b);
+}
+multiply(7,10)
